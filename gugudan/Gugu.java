87e3097990ae07dbff5d4d9b963122ebade5a71b
나 <@121.141.68.130>
2021-03-14 21:58:20 +0900

    input

--- gugudan/Gugu.java
+++ gugudan/Gugu.java
@@ -2,6 +2,8 @@ package gugudan;
 
 import java.util.Scanner;
 
+import javax.xml.ws.AsyncHandler;
+
 public class Gugu {
 
 	public static void main(String[] args) {
@@ -9,18 +11,25 @@ public class Gugu {
 		
 		int j;
 		int gob;
-		System.out.println("원하는 구구단 : ");
+		System.out.println("2~9까지 원하는 구구단 : ");
 		Scanner scanner=new Scanner(System.in);
 		int i=scanner.nextInt();
 
 		
 			System.out.println();
 			System.out.println(i+"단");
+			
+			if(i<2) {
+				System.out.println("값을 잘못 입력");
+			}else if(i>9){
+				System.out.println("값을 잘못 입력");
+
+			}else {
 			for (j = 1; j <=9; j++) {
 				gob=i*j;
 				System.out.print(i+"*"+j+"="+gob+" ");
 			}
-					    
+			}    
 		}
        
 	}
